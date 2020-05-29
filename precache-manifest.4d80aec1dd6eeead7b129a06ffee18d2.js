6cfcdc23035e73465da9ae5ca3a41ea34928a966
TomaszGa <tomasz.galina@gmail.com>
2020-05-29 22:37:37 +0200

    Updates

---
+++ precache-manifest.4d80aec1dd6eeead7b129a06ffee18d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72687c2c5b9c115af2d74b424a22977f",
+    "url": "/react-job-listings/index.html"
+  },
+  {
+    "revision": "9e3d87b86e61eb4edc61",
+    "url": "/react-job-listings/static/css/main.c07d72cb.chunk.css"
+  },
+  {
+    "revision": "f4cc1375b074f075d8ab",
+    "url": "/react-job-listings/static/js/2.00dba496.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-job-listings/static/js/2.00dba496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e3d87b86e61eb4edc61",
+    "url": "/react-job-listings/static/js/main.3e5f3907.chunk.js"
+  },
+  {
+    "revision": "e5fb1b2e6d5fd2af7015",
+    "url": "/react-job-listings/static/js/runtime-main.06dcdb00.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/react-job-listings/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/react-job-listings/static/media/bg-header-mobile.6b5ff5a0.svg"
+  }
+]);
